d75177cf8657da7faefa55fc35588337f1df6e56
Amnah Kurnia <59590227+IxoraVi@users.noreply.github.com>
2020-04-22 08:58:16 +0800

    Add files via upload

---
+++ Bangundatar.java
@@ -0,0 +1,17 @@
+package bangundatar;
+
+/**
+ *
+ * @author HP
+ */
+public class Bangundatar {
+    float luas(){
+        System.out.println("Menghitung luas bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+}
---
+++ Persegi.java
@@ -0,0 +1,18 @@
+package bangundatar;
+public class Persegi extends Bangundatar{
+    int sisi;
+    
+    public Persegi(int sisi) {
+        this.sisi = sisi;
+    }
+    
+    @Override
+    public float luas() {
+        return this.sisi * this.sisi;
+    }
+    
+    @Override
+    public float keliling(){
+        return this.sisi * 4;
+    }
+}
